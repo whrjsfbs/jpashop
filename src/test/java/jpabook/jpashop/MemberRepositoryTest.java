ab2c4bf556c3574efaf0b9ed85285053f804b658
whrjsfbs <whrjsfbs@gmail.com>
2021-07-03 20:49:37 +0900

    finish create entity

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -16,22 +16,22 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+//    @Autowired MemberRepository memberRepository;
 
-    @Test
-    @Transactional
+//    @Test
+//    @Transactional
 //    @Rollback(false)
     public void testMember() throws Exception {
         //given
-        Member member = new Member();
-        member.setUsername("memberA");
-        //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//        //when
+//        Long saveId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(saveId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
